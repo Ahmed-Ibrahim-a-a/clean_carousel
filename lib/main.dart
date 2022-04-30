0bbfb0bc1f31446f2a689b255faf54acb25b88fe
ZurraJanai <ZurraJanai@users.noreply.github.com>
2022-04-30 21:55:29 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:clean_carousel/view/carousel.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Carousel',
+      home: CarouselPage(),
+    );
+  }
+}
